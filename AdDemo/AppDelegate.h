ad71e4100e4c0bb932203a7b9a9b66d373dac0b8
Cnw <502886513@qq.com>
2017-02-14 13:14:24 +0800

    Initial Commit

---
+++ AdDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdDemo
+//
+//  Created by Cnw on 2017/2/14.
+//  Copyright © 2017年 Cnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdDemo
+//
+//  Created by Cnw on 2017/2/14.
+//  Copyright © 2017年 Cnw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
